5dcf1618efaf1080ed5bb2cda884aba629db0e98
Will Giger <bgigs37@gmail.com>
2020-10-04 14:48:19 -0400

    services working

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import { random } from 'core-js/fn/number';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
+})
+
+export class AppComponent {
+    // emoji = ['🎉', '😍', '😜', '👍'];
+    // activeEmoji: string;
+
+
+    // changeEmoji() {
+    //     this.activeEmoji = this.emoji[Math.floor(Math.random()*this.emoji.length)];
+    // }
+}
