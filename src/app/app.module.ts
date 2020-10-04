346a52817a3a76d55303246903122c4b1bf89a5c
Will Giger <bgigs37@gmail.com>
2020-10-04 14:59:36 -0400

    my bad wrong save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { EntryListComponent, EntryComponent, EntryService } from './entries';
+import { EntryListComponent, EntryComponent, EntryService, EntryCommentFormCompnent } from './entries';
 import { HttpModule } from '@angular/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryEntryService } from './backend';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
     imports: [BrowserModule,
         HttpModule,
-        InMemoryWebApiModule.forRoot(InMemoryEntryService)
+        InMemoryWebApiModule.forRoot(InMemoryEntryService),
+        FormsModule
     ],
     providers: [EntryService],
     declarations:
     [AppComponent,
     EntryListComponent,
-    EntryComponent
+    EntryComponent,
+    EntryCommentFormCompnent
     ],
     bootstrap: [AppComponent]
 })
